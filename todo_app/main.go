b10c29cff84cd3f40c3e1d0fb841182b641298a2
YugaNamba <yuga.namba@nbayu.com>
2024-04-29 09:42:45 +0900

    feat: 🎸 done todos

--- todo_app/main.go
+++ todo_app/main.go
@@ -11,9 +11,9 @@ func main() {
 	fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testtest"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.Password = "testtest2"
 	// fmt.Println(u)
 
 	// u.CreateUser()
@@ -32,5 +32,29 @@ func main() {
 
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
+
+	// user, _ := models.GetUser(2)
+	// fmt.Println(user)
+	// user.CreateTodo("Test Todo 1")
+
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("First Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(2)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(3)
+	t.DeleteTodo()
 }
 
